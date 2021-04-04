a022845ea7bbb1a3d1cae929c18025ad3181f9bf
jonah <jon4h@pm.me>
2021-04-04 10:21:17 +0200

    add unit tests

--- health_test.go
+++ health_test.go
@@ -4,10 +4,59 @@ import (
 	"testing"
 )
 
-func TestHealthReponse(t *testing.T) {
+// change url to valid
+func TestValidHealthReponseWithBasicClient(t *testing.T) {
 	client := NewBasicClient("https://docs.btcpayserver.com", "", "")
 	_, err := client.GetHealth()
 	if err != nil {
+		t.Error("Error while getting the health status: ", err)
+	}
+
+}
+
+// change url to valid
+func TestValidHealthReponseWithAPIClient(t *testing.T) {
+	client := NewClient("https://docs.btcpayserver.com", "")
+	_, err := client.GetHealth()
+	if err != nil {
+		t.Error("Error while getting the health status: ", err)
+	}
+
+}
+
+func TestInvalidHealthReponseWithBasicClient(t *testing.T) {
+	client := NewBasicClient("https://docs.btcpayserver.com", "", "")
+	_, err := client.GetHealth()
+	if err == nil {
+		// change error message
+		t.Error("Error while getting the health status: ", err)
+	}
+
+}
+func TestInvalidHealthReponseWithAPIClient(t *testing.T) {
+	client := NewClient("https://docs.btcpayserver.com", "")
+	_, err := client.GetHealth()
+	if err == nil {
+		// change error message
+		t.Error("Error while getting the health status: ", err)
+	}
+
+}
+
+func TestInvalidRequestHealthReponseWithBasicClient(t *testing.T) {
+	client := NewBasicClient("", "", "")
+	_, err := client.GetHealth()
+	if err == nil {
+		// change error message
+		t.Error("Error while getting the health status: ", err)
+	}
+
+}
+func TestInvalidRequestHealthReponseWithAPIClient(t *testing.T) {
+	client := NewClient("", "")
+	_, err := client.GetHealth()
+	if err == nil {
+		// change error message
 		t.Error("Error while getting the health status: ", err)
 	}
 

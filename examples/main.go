c9efff95fedde3548087ae87018e299c3fcb422d
jon4h <admin@psytux.com>
2021-04-05 00:23:09 +0200

    testing

--- examples/main.go
+++ examples/main.go
@@ -4,17 +4,47 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/jon4hz/go-btcpay"
 	"github.com/jon4hz/go-btcpay/examples/config"
 )
 
 func main() {
+	ctx := context.Background()
 	config, err := config.ReadConf("config/config.yml")
 	if err != nil {
 		log.Fatal(err)
 	}
 	client := btcpay.NewBasicClient(config.BTCPay.URL, config.BTCPay.Username, config.BTCPay.Password)
-	ctx := context.Background()
-	fmt.Println(client.CreateAPIKey(&btcpay.CreateAPIKeyRequest{}, ctx))
+	fmt.Println(client.GetHealth(ctx))
+
+	// create new APIKey
+	apiKey, err := client.CreateAPIKey(&btcpay.CreateAPIKeyRequest{
+		Permissions: []btcpay.Authentication{client.CreateRestrictedKey(btcpay.BTCPayStoreCanviewinvoices, "66tU3WhCAcsbocA3EmUXHE96XsoVQjWMUiTp3s6LLYAn")},
+	}, ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// delete the new APIKey
+	err = client.RevokeAPIKey(&btcpay.RevokeAPIKeyRequest{APIKey: apiKey.APIKey}, ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// create new APIKey
+	apiKey, err = client.CreateAPIKey(&btcpay.CreateAPIKeyRequest{
+		Permissions: []btcpay.Authentication{client.CreateRestrictedKey(btcpay.BTCPayStoreCanviewinvoices, "66tU3WhCAcsbocA3EmUXHE96XsoVQjWMUiTp3s6LLYAn")},
+	}, ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// add APIKey to client
+	client.APIKey = apiKey.APIKey
+	fmt.Println(client.GetCurrentAPIKey(ctx))
+	time.Sleep(10 * time.Second)
+	client.RevokeCurrentAPIKey(ctx)
+
 }

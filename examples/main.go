d09d2a089082605fa6644c543e1e8b17f0dadfc7
jon4h <admin@psytux.com>
2021-04-05 06:03:58 +0200

    wip: invoices

--- examples/main.go
+++ examples/main.go
@@ -1,5 +1,7 @@
 package main
 
+// WIP
+
 import (
 	"context"
 	"fmt"
@@ -18,35 +20,66 @@ func main() {
 	}
 	client := btcpay.NewBasicClient(config.BTCPay.URL, config.BTCPay.Username, config.BTCPay.Password)
 	fmt.Println(client.GetHealth(ctx))
-
 	fmt.Println(client.GetServerInfo(ctx))
+
+	// get store id
+	stores, _, err := client.GetStores(ctx)
+	if err != nil {
+		panic(err)
+	}
+	storeID := getStoreID(stores)
+	if len(storeID) > 0 {
+		fmt.Println(storeID)
+		invoice, _, err := client.CreateInvoice(&storeID, &btcpay.CreateInvoiceRequest{Amount: "11", Currency: "USD", Metadata: btcpay.InvoiceMetadata{"test": "asdf", "test2": "aaaa"}}, ctx)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(invoice)
+		client.UpdateInvoice(&storeID, &invoice.ID, &btcpay.InvoiceUpdate{Metadata: btcpay.InvoiceMetadata{"test3": "ccccc"}}, ctx)
+		fmt.Println(client.GetInvoices(&storeID, ctx))
+		client.ArchiveInvoice(&storeID, &invoice.ID, ctx)
+	}
+
+}
+
+func getStoreID(stores []*btcpay.StoreResponse) btcpay.StoreID {
+	for _, v := range stores {
+		if v.Name == "test01" {
+			return v.ID
+		}
+	}
+	return ""
+}
+
+func createAndDeleteAPIKey(client *btcpay.Client) {
 
 	// create new APIKey
 	apiKey, _, err := client.CreateAPIKey(&btcpay.CreateAPIKeyRequest{
-		Permissions: []btcpay.Permission{client.CreateRestrictedKey(btcpay.BTCPayStoreCanviewinvoices, "66tU3WhCAcsbocA3EmUXHE96XsoVQjWMUiTp3s6LLYAn")},
-	}, ctx)
+		Permissions: []btcpay.Permission{client.CreateRestrictedKey(btcpay.BTCPayStoreCanviewinvoices, btcpay.StoreID("66tU3WhCAcsbocA3EmUXHE96XsoVQjWMUiTp3s6LLYAn"))},
+	}, context.Background())
 	if err != nil {
 		panic(err)
 	}
 
 	// delete the new APIKey
-	_, err = client.RevokeAPIKey(apiKey.APIKey, ctx)
+	_, err = client.RevokeAPIKey(&apiKey.APIKey, context.Background())
 	if err != nil {
 		panic(err)
 	}
+}
 
+func createAndDeleteCurrentAPIKey(client *btcpay.Client) {
 	// create new APIKey
-	apiKey, _, err = client.CreateAPIKey(&btcpay.CreateAPIKeyRequest{
-		Permissions: []btcpay.Permission{client.CreateRestrictedKey(btcpay.BTCPayStoreCanviewinvoices, "66tU3WhCAcsbocA3EmUXHE96XsoVQjWMUiTp3s6LLYAn")},
-	}, ctx)
+	apiKey, _, err := client.CreateAPIKey(&btcpay.CreateAPIKeyRequest{
+		Permissions: []btcpay.Permission{client.CreateRestrictedKey(btcpay.BTCPayStoreCanviewinvoices, btcpay.StoreID("66tU3WhCAcsbocA3EmUXHE96XsoVQjWMUiTp3s6LLYAn"))},
+	}, context.Background())
 	if err != nil {
 		panic(err)
 	}
 
 	// add APIKey to client
 	client.APIKey = apiKey.APIKey
-	fmt.Println(client.GetCurrentAPIKey(ctx))
+	fmt.Println(client.GetCurrentAPIKey(context.Background()))
 	time.Sleep(10 * time.Second)
-	client.RevokeCurrentAPIKey(ctx)
-
+	client.RevokeCurrentAPIKey(context.Background())
 }
--- invoices.go
+++ invoices.go
@@ -3,6 +3,7 @@ package btcpay
 // WIP
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -11,23 +12,21 @@ import (
 
 type InvoiceID string
 
-type InvoicesResponse struct {
-	Amount           string          `json:"amount,omitempty"`
-	Currency         string          `json:"currency,omitempty"`
-	Metadata         InvoiceMetadata `json:"metadata,omitempty"`
-	Checkout         InvoiceCheckout `json:"checkout,omitempty"`
-	ID               InvoiceID       `json:"id"`
-	CheckoutLink     string          `json:"checkoutLink"`
-	CreatedTime      int64           `json:"createdTime"`
-	ExpirationTime   int64           `json:"expirationTime"`
-	MonitoringTime   int64           `json:"monitoringTime"`
-	Status           string          `json:"status"`
-	AdditionalStatus string          `json:"additionalStatus"`
+type InvoiceResponse struct {
+	Amount           string                  `json:"amount,omitempty"`
+	Currency         string                  `json:"currency,omitempty"`
+	Metadata         InvoiceMetadata         `json:"metadata,omitempty"`
+	Checkout         InvoiceCheckout         `json:"checkout,omitempty"`
+	ID               InvoiceID               `json:"id"`
+	CheckoutLink     string                  `json:"checkoutLink"`
+	CreatedTime      int64                   `json:"createdTime"`
+	ExpirationTime   int64                   `json:"expirationTime"`
+	MonitoringTime   int64                   `json:"monitoringTime"`
+	Status           InvoiceStatus           `json:"status"`
+	AdditionalStatus InvoiceAdditionalStatus `json:"additionalStatus"`
 }
 
-type InvoiceMetadata struct {
-	OrderID string `json:"orderId,omitempty"`
-}
+type InvoiceMetadata map[string]string
 
 type SpeedPolicy string
 
@@ -36,6 +35,27 @@ const (
 	MediumSpeed    SpeedPolicy = "MediumSpeed"
 	LowMediumSpeed SpeedPolicy = "LowMediumSpeed"
 	LowSpeed       SpeedPolicy = "LowSpeed"
+)
+
+type InvoiceStatus string
+
+const (
+	New        InvoiceStatus = "New"
+	Processing InvoiceStatus = "Processing"
+	Expired    InvoiceStatus = "Expired"
+	Invalid    InvoiceStatus = "Invalid"
+	Settled    InvoiceStatus = "Settled"
+)
+
+type InvoiceAdditionalStatus string
+
+const (
+	None        InvoiceAdditionalStatus = "None"
+	PaidLate    InvoiceAdditionalStatus = "PaidLate"
+	PaidPartial InvoiceAdditionalStatus = "PaidPartial"
+	Marked      InvoiceAdditionalStatus = "Marked"
+	AddInvalid  InvoiceAdditionalStatus = "Invalid"
+	PaidOver    InvoiceAdditionalStatus = "PaidOver"
 )
 
 type InvoiceCheckout struct {
@@ -48,10 +68,8 @@ type InvoiceCheckout struct {
 	DefaultLanguage   string      `json:"defaultLanguage,omitempty"`
 }
 
-type StoreID string
-
-func (c *Client) GetInvoices(storeID StoreID, ctx context.Context) (*[]InvoicesResponse, int, error) {
-	endpoint := fmt.Sprintf("%s/api/v1/stores/%s/invoices", c.URL, storeID)
+func (c *Client) GetInvoices(storeID *StoreID, ctx context.Context) (*[]InvoiceResponse, int, error) {
+	endpoint := fmt.Sprintf("%s/api/v1/stores/%s/invoices", c.URL, *storeID)
 	req, err := http.NewRequestWithContext(ctx, "GET", endpoint, nil)
 	if err != nil {
 		return nil, 0, err
@@ -60,13 +78,13 @@ func (c *Client) GetInvoices(storeID StoreID, ctx context.Context) (*[]InvoicesR
 	if err != nil {
 		return nil, statusCode, err
 	}
-	var data []InvoicesResponse
-	err = json.Unmarshal(bytes, &data)
+	var dataRes []InvoiceResponse
+	err = json.Unmarshal(bytes, &dataRes)
 	if err != nil {
 		return nil, 0, err
 	}
 
-	return &data, statusCode, nil
+	return &dataRes, statusCode, nil
 }
 
 type CreateInvoiceRequest struct {
@@ -74,4 +92,87 @@ type CreateInvoiceRequest struct {
 	Currency        string          `json:"currency,omitempty"`
 	Metadata        InvoiceMetadata `json:"metadata,omitempty"`
 	InvoiceCheckout InvoiceCheckout `json:"checkout,omitempty"`
+}
+
+func (c *Client) CreateInvoice(storeID *StoreID, invoiceRequest *CreateInvoiceRequest, ctx context.Context) (*InvoiceResponse, int, error) {
+	endpoint := fmt.Sprintf("%s/api/v1/stores/%s/invoices", c.URL, *storeID)
+	dataReq, err := json.Marshal(invoiceRequest)
+	if err != nil {
+		return nil, 0, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", endpoint, bytes.NewBuffer(dataReq))
+	if err != nil {
+		return nil, 0, err
+	}
+	bytes, statusCode, err := c.doRequest(req)
+	if err != nil {
+		return nil, statusCode, err
+	}
+	var dataRes InvoiceResponse
+	err = json.Unmarshal(bytes, &dataRes)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return &dataRes, statusCode, nil
+}
+
+func (c *Client) GetInvoice(storeID *StoreID, invoiceID *InvoiceID, ctx context.Context) (*InvoiceResponse, int, error) {
+	endpoint := fmt.Sprintf("%s/api/v1/stores/%s/invoices/%s", c.URL, *storeID, *invoiceID)
+	req, err := http.NewRequestWithContext(ctx, "GET", endpoint, nil)
+	if err != nil {
+		return nil, 0, err
+	}
+	bytes, statusCode, err := c.doRequest(req)
+	if err != nil {
+		return nil, statusCode, err
+	}
+	var dataRes InvoiceResponse
+	err = json.Unmarshal(bytes, &dataRes)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return &dataRes, statusCode, nil
+}
+
+func (c *Client) ArchiveInvoice(storeID *StoreID, invoiceID *InvoiceID, ctx context.Context) (int, error) {
+	endpoint := fmt.Sprintf("%s/api/v1/stores/%s/invoices/%s", c.URL, *storeID, *invoiceID)
+	req, err := http.NewRequestWithContext(ctx, "DELETE", endpoint, nil)
+	if err != nil {
+		return 0, err
+	}
+	_, statusCode, err := c.doRequest(req)
+	if err != nil {
+		return statusCode, err
+	}
+
+	return statusCode, nil
+}
+
+type InvoiceUpdate struct {
+	Metadata InvoiceMetadata `json:"metadata,omitempty"`
+}
+
+func (c *Client) UpdateInvoice(storeID *StoreID, invoiceID *InvoiceID, invoiceUpdate *InvoiceUpdate, ctx context.Context) (*InvoiceResponse, int, error) {
+	endpoint := fmt.Sprintf("%s/api/v1/stores/%s/invoices/%s", c.URL, *storeID, *invoiceID)
+	dataReq, err := json.Marshal(invoiceUpdate)
+	if err != nil {
+		return nil, 0, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "PUT", endpoint, bytes.NewBuffer(dataReq))
+	if err != nil {
+		return nil, 0, err
+	}
+	bytes, statusCode, err := c.doRequest(req)
+	if err != nil {
+		return nil, statusCode, err
+	}
+	var dataRes InvoiceResponse
+	err = json.Unmarshal(bytes, &dataRes)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return &dataRes, statusCode, nil
 }
